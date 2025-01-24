c2daaae25573a2cf7a2b62044a6b78b155267658
Es'kin Vasiliy Alekseevich <vasiliyeskin@yandex.ru>
2025-01-24 21:59:14 +0300

    Update plotting.jl

--- src/plotting.jl
+++ src/plotting.jl
@@ -199,7 +199,7 @@ function getmesh(a_density, iso_value,x)
 	    pointsaspoints[i] = Point3f(points[i][1], points[i][2], points[i][3])
 	end
 
-	return GeometryBasics.MetaMesh(pointsaspoints, facesagain)
+	return GeometryBasics.Mesh(metamesh(pointsaspoints), facesagain)
 
 end
 
